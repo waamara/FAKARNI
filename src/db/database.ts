876e01fb439fc74666e76464e93fc8f0f9625a02
Aymen Berbiche <138572701+waamara@users.noreply.github.com>
2026-07-23 20:45:50 +0100

    added vehicules table in the db

--- src/db/database.ts
+++ src/db/database.ts
@@ -1,1 +1,17 @@
 import * as SQLite from 'expo-sqlite';  
+
+const db = SQLite.openDatabaseSync('fakarni.db');  
+export function initDatabase() {
+    db.execSync(` 
+        CREATE TABLE IF NOT EXISTS vehicles (
+            id TEXT PRIMARY KEY,
+            name TEXT NOT NULL, 
+            brand TEXT NOT NULL,
+            model TEXT, 
+            year INTEGER, 
+            current_km INTEGER NOT NULL DEFAULT 0, 
+            created_at TEXT NOT NULL 
+        ); 
+    
+    
+}
