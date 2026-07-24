5152858a7a54b7d50d0c827a5e696d1e2def3403
Aymen Berbiche <138572701+waamara@users.noreply.github.com>
2026-07-24 19:08:52 +0100

    commit all changes on layout.tsx

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,15 +1,19 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useColorScheme } from 'react-native';
-import { useEffect } from 'react' ;
+import { useEffect } from 'react';
 import { AnimatedSplashOverlay } from '@/components/animated-icon';
-import AppTabs from '@/components/app-tabs'; 
-import { initDatabase } from "@/db/database" ;
+import AppTabs from '@/components/app-tabs';
+import { initDatabase } from "@/db/database";
 
 SplashScreen.preventAutoHideAsync();
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
+  useEffect(() => {
+    initDatabase();
+  }, []);
+  
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <AnimatedSplashOverlay />
