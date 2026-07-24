25e5ff4e90851e3f098673e53d1b8094b9b5e90a
Aymen Berbiche <138572701+waamara@users.noreply.github.com>
2026-07-24 19:07:24 +0100

    modification sur layout.tsx

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,9 +1,10 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useColorScheme } from 'react-native';
-
+import { useEffect } from 'react' ;
 import { AnimatedSplashOverlay } from '@/components/animated-icon';
-import AppTabs from '@/components/app-tabs';
+import AppTabs from '@/components/app-tabs'; 
+import { initDatabase } from "@/db/database" ;
 
 SplashScreen.preventAutoHideAsync();
 
