defe56e48c023c94666fe03d81eada0d31e38e7d
Aymen Berbiche <138572701+waamara@users.noreply.github.com>
2026-07-27 21:55:09 +0100

    commit

--- src/db/maintenance.ts
+++ src/db/maintenance.ts
@@ -1,2 +1,14 @@
 import db from './database' 
 
+export interface MaintenanceType {
+    id: string;
+    vehicle_id: string;
+    name: string;
+    icon?: string;
+    interval_km: number;
+    alert_before_km: number;
+    last_service_km: number;
+    last_service_date?: string;
+    is_costum: number;
+}
+
