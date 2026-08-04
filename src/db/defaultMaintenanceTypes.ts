80cd5b2d7abbe3772c26bb8a25ff660fdc4b58cc
Aymen Berbiche <138572701+waamara@users.noreply.github.com>
2026-08-04 13:50:34 +0100

    added the types

--- src/db/defaultMaintenanceTypes.ts
+++ src/db/defaultMaintenanceTypes.ts
@@ -1,4 +1,11 @@
 import { createMaintenanceType } from "./maintenance" ; 
 
 
+const DEFAULT_TYPES = [ 
+    { name: 'Vidange' , icon: 'oil-can' , interval_km: 10000 }, 
+    { name: 'Pneus / rotation' , icon: 'titre' , interval_km:20000 },
+    { name: "Filtre a air" , icon: 'air-filter' , interval_km:15000 },
+    { name: 'Courroie' , icon: 'belt' , interval_km:60000 },
+]
+
 
