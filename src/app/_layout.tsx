5c986a29aa7fbf63ff53ce7f86b49aeac1d3653a
Aymen Berbiche <138572701+waamara@users.noreply.github.com>
2026-08-13 22:56:48 +0100

    commit

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -22,3 +22,13 @@ export default function TabLayout() {
     </ThemeProvider>
   );
 }
+
+import React from 'react'
+
+const _layout = () => {
+  return (
+    <div>_layout</div>
+  )
+}
+
+export default _layout
