d5e154b1aa1b1abe212220b7efe0e37e8a664434
Aymen Berbiche <138572701+waamara@users.noreply.github.com>
2026-09-06 23:35:00 +0100

    commit

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,2 +1,3 @@
 import { useCallback, useState } from "react"; 
 import { View , Text, FlatList, TouchableOpacity } from 'react-native';
+import { SafeAreaView} from 'react-native-safe-area-context'; 
