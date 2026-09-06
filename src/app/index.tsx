fe20a8455fcad91c05842323aefcc6195a71e039
Aymen Berbiche <138572701+waamara@users.noreply.github.com>
2026-09-06 23:39:32 +0100

    commit

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,3 +1,8 @@
 import { useCallback, useState } from "react"; 
 import { View , Text, FlatList, TouchableOpacity } from 'react-native';
 import { SafeAreaView} from 'react-native-safe-area-context'; 
+import VehicleCard  from "../components/VehicleCard"; 
+import { getAllVehicles } from "../db/vehicles"; 
+import { getMaintenanceTypesByVehicle } from "../db/maintenance"; 
+import { getMaintenanceStatus, MaintenanceStatus } from "../utiles/maintenanceStatus"; 
+
